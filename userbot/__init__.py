fa8717171f1ea61265fc271ab856fd461cc24a1f
ikhsantarjo <97121826+ikhsantarjo@users.noreply.github.com>
2022-01-05 01:40:23 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -120,7 +120,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/DIORrios285/DIOR-UBOT")
+    "https://github.com/ikhsantarjo/JOO-UBOT")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "DIOR-UBOT")
 
